1b2725b95fa01bb521fc4fee49b06e324d4672eb
jodadiz <jodadi04@gmail.com>
2021-11-27 01:08:38 -0600

    descripcion

--- laboratorio1.c
+++ laboratorio1.c
@@ -1,3 +1,11 @@
+/*
+Jose David Diaz Perez C02638
+Por medio de la entrada de dos digitos se calcula
+con ellos el residuo el cual es el que nos va a
+indicar si se realiza el MCD cuando su residuo
+sea 0 y de tambien se añaden algunas condiciones
+*/
+
 #include <stdio.h>
 
 int main() {
